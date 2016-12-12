042c768f2cd4ba8777bf32a8b86d279abcc6f983
Rias Van der Veken <hello@rias.be>
2016-12-12 10:58:08 +0100

    Refactor view logic to controller

--- SitemapController.php
+++ SitemapController.php
@@ -2,6 +2,8 @@
 
 namespace Statamic\Addons\Sitemap;
 
+use Statamic\API\Content;
+use Statamic\Exceptions\InvalidEntryTypeException;
 use Statamic\Extend\Controller;
 
 class SitemapController extends Controller
@@ -13,6 +15,25 @@ class SitemapController extends Controller
      */
     public function index()
     {
-        return response()->make($this->view('index'), '200')->header('Content-Type', 'text/xml');
+        $content = Content::all();
+
+        $content = $content->filter(function ($entry) {
+            return $entry->published();
+        })->map(function ($entry) {
+
+            try {
+                if (method_exists($entry, 'date')) {
+                    $date = $entry->date();
+                }
+            } catch (InvalidEntryTypeException $e) {
+            }
+
+            return [
+                'url' => $entry->absoluteUrl(),
+                'date' => isset($date) ? $date : null
+            ];
+        });
+
+        return response()->make($this->view('index', compact('content')), '200')->header('Content-Type', 'text/xml');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.google.com/schemas/sitemap/0.90">
-    @foreach (\Statamic\API\Content::all() as $content)
-        @if ($content->published())
-            <url>
-                <loc>{{ $content->absoluteUrl() }}</loc>
-                <?php
-                    $date = null;
-                    try {
-                        if (method_exists($content, 'date')) {
-                            $date = $content->date();
-                        }
-                    } catch (ErrorException $e) {
-                    } catch (Statamic\Exceptions\InvalidEntryTypeException $e) {
-                    }
-                ?>
-                @if (!is_null($date))
-                    <lastmod>
-                        {{ $date->toAtomString() }}
-                    </lastmod>
-                @endif
-            </url>
-        @endif
+    @foreach ($content as $entry)
+        <url>
+            <loc>{{ $entry['url'] }}</loc>
+            @if (!is_null($entry['date']))
+                <lastmod>
+                    {{ $entry['date']->toAtomString() }}
+                </lastmod>
+            @endif
+        </url>
     @endforeach
 </urlset>
