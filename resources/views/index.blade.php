8c7ea96d864e4b41991a3244a09adbeb49caa6ed
Rias Van der Veken <hello@rias.be>
2017-01-03 16:13:04 +0100

    Fix namespace

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.google.com/schemas/sitemap/0.90">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($content as $entry)
         <url>
             <loc>{{ $entry['url'] }}</loc>
