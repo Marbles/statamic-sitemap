e9b3a77468d0a3c46e7556c259c617766da79d19
Rémy <git@rrelmy.ch>
2017-08-16 15:47:39 +0200

    add xhtml context for multi language sitemaps

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     @foreach ($content as $entry)
         <url>
             <loc>{{ $entry['url'] }}</loc>
